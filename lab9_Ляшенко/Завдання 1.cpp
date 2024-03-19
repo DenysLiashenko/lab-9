9df4cea1f999bbc41aa0232d1df9228514bae302
DenysLiashenko <liashenko.denys.romanovych@gmail.com>
2024-03-19 20:39:34 +0100

    1st

---
+++ lab9_Ляшенко/Завдання 1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream> //бібліотека для роботи операторів введення / виведення
+#include <math.h> //бібліотека для використання математичних функцій
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "ukr"); //дозволити використання української мови
+	const int n = 3, m = 3;
+	int i, k, a[n][m], min=0, max=0, rmax = 0, rmin = 0,x;
+
+	for (i = 0; i < n; i++)
+	{
+		for (k = 0; k < m; k++)
+		{
+			cin >> a[i][k];
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (k = 0; k < m; k++)
+		{
+			if (max < a[i][k])
+			{
+				rmax = i;
+			}
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (k = 0; k < m; k++)
+		{
+			if (min > a[i][k])
+			{
+				rmin = i;
+			}
+		}
+	}
+
+	for (k = 0; k < n; k++)
+	{
+		x = a[rmax][k];
+		a[rmax][k] = a[rmin][k];
+		a[rmin][k] = x;
+	}
+
+	cout << endl;
+
+	for (i = 0; i < n; i++)
+	{
+		for (k = 0; k < m; k++)
+		{
+			cout << a[i][k] << " ";
+		}
+		cout << endl;
+	}
+}
---
+++ Завдання 2/Завдання 2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream> //бібліотека для роботи операторів введення / виведення
+#include <math.h> //бібліотека для використання математичних функцій
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "ukr"); //дозволити використання української мови
+	const int n = 3, m = 3;
+	const int x = n * m;
+	int i, k, a[n][m], b[x], j, y = 0, c = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		for (k = 0; k < m; k++)
+		{
+			cin >> a[i][k];
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (k = 0; k < m; k++)
+		{
+			for (j = 2; j < a[i][k]; j++)
+			{
+				if (a[i][k] % j == 0)
+				{
+					y++;
+				}
+			}
+
+			if (y == 0)
+			{
+				b[c] = a[i][k];
+				c++;
+			}
+			y = 0;
+		}
+	}
+
+	for (c = 0; c < x; c++)
+	{
+		cout << b[c] << " ";
+	}
+
+}
